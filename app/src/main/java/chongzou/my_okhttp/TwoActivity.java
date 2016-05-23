c3fc0f48987522cdf2edd861a4bce631d648fbe9
Chen1105695211 <1105695211@qq.com>
2016-05-23 15:20:34 +0800

    这是two类

--- app/src/main/java/chongzou/my_okhttp/TwoActivity.java
+++ app/src/main/java/chongzou/my_okhttp/TwoActivity.java
@@ -2,6 +2,7 @@ package chongzou.my_okhttp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class TwoActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class TwoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
+        //cehnpan
+        Toast.makeText(TwoActivity.this,"这是添加",Toast.LENGTH_SHORT).show();
     }
 }
