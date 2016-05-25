3e273101f884a7a50de564b87a5d645eb4c558a0
Chen1105695211 <1105695211@qq.com>
2016-05-25 16:57:37 +0800

    nihao

---
+++ app/src/main/java/chongzou/my/FourActivity.java
@@ -0,0 +1,15 @@
+package chongzou.my;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import chongzou.my_okhttp.R;
+
+public class FourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_four);
+    }
+}
