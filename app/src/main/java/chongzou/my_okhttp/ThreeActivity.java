e6413a3710a30c9a1f32c87bc9b58f490d108ae1
Chen1105695211 <1105695211@qq.com>
2016-05-25 16:11:25 +0800

    wefa

--- app/src/main/java/chongzou/my_okhttp/ThreeActivity.java
+++ app/src/main/java/chongzou/my_okhttp/ThreeActivity.java
@@ -15,5 +15,8 @@ public class ThreeActivity extends AppCompatActivity{
         Toast.makeText(ThreeActivity.this, "这是添", Toast.LENGTH_SHORT).show();
         Toast.makeText(ThreeActivity.this, "这是添", Toast.LENGTH_SHORT).show();
         Toast.makeText(ThreeActivity.this, "这是胜多负少添", Toast.LENGTH_SHORT).show();
+        Toast.makeText(ThreeActivity.this, "这添", Toast.LENGTH_SHORT).show();
+        Toast.makeText(ThreeActivity.this, "这是胜负少添", Toast.LENGTH_SHORT).show();
+
     }
 }
