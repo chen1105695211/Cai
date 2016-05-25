90e68e5f07a9dec879b7eef40078c6e0bf1eb3a2
Chen1105695211 <1105695211@qq.com>
2016-05-25 11:33:55 +0800

    第三个

--- app/src/main/java/chongzou/my_okhttp/MainActivity.java
+++ app/src/main/java/chongzou/my_okhttp/MainActivity.java
@@ -3,6 +3,8 @@ package chongzou.my_okhttp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //创建okHttpClient对象
+        Toast.makeText(MainActivity.this, "这是添加", Toast.LENGTH_SHORT).show();
         OkHttpClient okHttpClient=new OkHttpClient();
         //创建一个Request
         final Request request = new Request.Builder().url("http://api.fang.anjuke.com/m/android/1.3/shouye/recInfosV3/?city_id=14&lat=40.04652&lng=116.306033&api_key=androidkey&sig=9317e9634b5fbc16078ab07abb6661c5&macid=45cd2478331b184ff0e15f29aaa89e3e&app=a-ajk&_pid=11738&o=PE-TL10-user+4.4.2+HuaweiPE-TL10+CHNC00B260+ota-rel-keys%2Crelease-keys&from=mobile&m=Android-PE-TL10&cv=9.5.1&cid=" +
---
+++ app/src/main/java/chongzou/my_okhttp/ThreeActivity.java
@@ -0,0 +1,15 @@
+package chongzou.my_okhttp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class ThreeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three);
+        Toast.makeText(ThreeActivity.this, "这是添加", Toast.LENGTH_SHORT).show();
+    }
+}
